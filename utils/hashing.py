4dab330a3cc2187b2d4b9c37356d50f095b554c0
maisha-maliha <aroralove303@gmail.com>
2025-04-17 00:17:03 +0600

    feature: added password hash and matching function

---
+++ utils/hashing.py
@@ -0,0 +1,21 @@
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """Hashing the password and returning it"""
+    hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hash.decode()  # salt round 12 by default
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    """match the password with hashed password by unhashing it"""
+    return bcrypt.checkpw(password.encode(), hashed_password.encode()) # match password
+
+
+
+
+
+
+
+
+
