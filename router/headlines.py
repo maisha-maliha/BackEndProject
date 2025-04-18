af9be3f1a8e9018360e88d3b6f7e97e8b4b392f9
maisha-maliha <aroralove303@gmail.com>
2025-04-18 14:47:54 +0600

    feature: source_id and country_code api done

--- router/headlines.py
+++ router/headlines.py
@@ -1,20 +1,41 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
+from typing import Annotated
 from schemas import Filter
+from dotenv import load_dotenv
+import requests
+import os
 
 router = APIRouter(prefix="/news/headlines", tags=["news"])
 
+# load all environment variable
+load_dotenv()
+# NEWS API
+NEWSAPI_KEY = os.getenv("NEWSAPI_KEY")
+
 
 @router.get("/filter/")
-def source_id_headlines(filter: Filter):
+def country_source_headlines(filter: Filter):
     """Fetch top headlines by filtering both country and source
     (use query parameters country and source)"""
 
 
 @router.get("/country/{country_code}")
-def country_code_headlines(country_code):
+def country_code_headlines(country_code: Annotated[str, Path()]):
     """Fetch top headlines by country"""
+    url = f"https://newsapi.org/v2/top-headlines?country={country_code}"
+    response = requests.get(
+        url,
+        headers={"x-api-key": NEWSAPI_KEY},
+    )
+    return {"response": response.json()}
 
 
 @router.get("/source/{source_id}")
 def source_id_headlines(source_id):
     """Fetch top headlines by source"""
+    url = f"https://newsapi.org/v2/top-headlines?sources={source_id}"
+    response = requests.get(
+        url,
+        headers={"x-api-key": NEWSAPI_KEY},
+    )
+    return {"response": response.json()}
--- schemas/filter.py
+++ schemas/filter.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel
 
 # for filtering top headlines by country and source
 class Filter(BaseModel):
-    country_code: str
+    country: str
     source: str
